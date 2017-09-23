5e88d3ebe0c8f2718bf65aad7bf1993c1fc5cf6e
isaias <isaiasdelgado007@gmail.com>
2017-09-23 15:28:55 -0600

    Se actualiza el script y se agregan dos get de informacion sobre el usuario

--- src/tmp_script.sh
+++ src/tmp_script.sh
@@ -8,12 +8,32 @@ RESET="\033[0m"
 
 #revisar que sea el usuario root 
 
-if [[ 0 != $UID ]]
+if [[ 0 == $UID ]]
 then 
 	#inicio el codigo con red, luego los elimino con reset
 	echo -e "${RED}USTED DEBE SER ROOT PARA CORRER ESTE SCRIPT${RESET}"
 	exit 1
 fi
 
-echo hola $USER
-	
+#Get de la informacion 
+
+echo -en "Escriba su nombre completo > " 
+read NOMBRE
+echo -en "Escriba su correo electronico > "
+read EMAIL
+#variables de email y nombre
+
+
+
+
+
+echo -en "hola $NOMBRE, este es su correo: [${GREEN}$EMAIL${RESET}] ? [s/n] "
+read RESP
+
+if [[ s == $RESP ]]
+then
+
+	echo HOLA $NOMBRE, su correo es $EMAIL
+fi
+
+
